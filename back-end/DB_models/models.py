d1f8e89d769ea230f84be6a9af0e04a112f721fb
AI 23 <mac@MacBook-Pro-mak.local>
2025-03-02 19:56:22 +0200

    Fixed endpoint

--- back-end/DB_models/models.py
+++ back-end/DB_models/models.py
@@ -1,20 +1,43 @@
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 from django.core.validators import MaxValueValidator
+from django.contrib.auth.models import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, age, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email обов'язковий")
+        if not first_name or not last_name:
+            raise ValueError("Ім'я та прізвище обов'язкові")
+
+        email = self.normalize_email(email)
+
+        user = self.model(
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            age=age,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, age, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, first_name, last_name, age, password, **extra_fields)
 
 class User(AbstractUser):
-    username = None  # Видаляємо username
+    username = None
     first_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
     last_name = models.CharField(max_length=50)
     age = models.IntegerField(default=0)
-    phone_number = models.IntegerField(
-        validators=[MaxValueValidator(9999999999)],
-        blank=True,
-        null=True
-    )
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
     is_seller = models.BooleanField(default=False)
 
-    # Унікальний ідентифікатор користувача
     user_id = models.CharField(max_length=100, unique=True)
 
     groups = models.ManyToManyField(Group, related_name="custom_user_groups", blank=True)
@@ -22,16 +45,20 @@ class User(AbstractUser):
         Permission, related_name="custom_user_permissions", blank=True
     )
 
-    USERNAME_FIELD = 'user_id'  # Використовуємо user_id як основний ідентифікатор
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name', 'age']
+
+    objects = CustomUserManager()
 
     def save(self, *args, **kwargs):
         if not self.user_id:
+            super().save(*args, **kwargs)  # Перше збереження для створення `pk`
             self.user_id = f"{self.first_name.lower()}_{self.last_name.lower()}_{self.pk}"
         super().save(*args, **kwargs)
 
     def __str__(self):
         return self.user_id
+
 
 class Talent_dsc(models.Model):
     prof = models.CharField(max_length=100)
